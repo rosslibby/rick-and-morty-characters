6d92b6de103b446841447d853851cff51c910574
Ross Libby <rosslibby92@gmail.com>
2023-10-30 16:51:53 -0400

    fetch episodes from API

--- src/app/api/episodes/route.ts
+++ src/app/api/episodes/route.ts
@@ -1,1 +1,19 @@
 import { NextResponse } from 'next/server'
+import { ENDPOINT } from '../constants'
+
+export async function GET(request: Request) {
+  try {
+    const response = await (await fetch(`${ENDPOINT}/episode`)).json()
+
+    return NextResponse.json(
+      response.results,
+      {
+        status: 200,
+      },
+    )
+  } catch(err) {
+    console.error('There was a problem fetching episodes:', err)
+
+    return NextResponse.error()
+  }
+}
