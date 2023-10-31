fa10fdb742bf5bf80d6d897cc9d9f548125c33c6
Ross Libby <rosslibby92@gmail.com>
2023-10-30 20:38:28 -0400

    load data on app load; utilize Store instead of Navigation for data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Navigation from 'components/navigation'
 import Store from './api'
 import './globals.css'
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 'use client'
 
-import Image from 'next/image'
 import styles from './page.module.css'
 import Episodes from 'components/episodes'
 import Characters from 'components/characters'
+import { useCallback, useContext, useEffect } from 'react'
+import { storeCtx } from './api'
+import { useApi } from './api/hooks/api'
 
 export default function Home() {
+  const { characters, episodes, loading } = useContext(storeCtx)
+  const { loadCharacters, loadEpisodes } = useApi()
+  const init = useCallback(() => {
+    if (!characters.length && !episodes.length && !loading) {
+      loadCharacters()
+      loadEpisodes()
+    }
+  }, [characters, episodes, loading, loadCharacters, loadEpisodes])
+
+  useEffect(() => {
+    if (!characters.length && !episodes.length && !loading) {
+      init()
+    }
+  }, [characters, episodes, init, loading])
 
   return (
     <main className={styles.main}>
